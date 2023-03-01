353472a2857119ef637a52d5db5eb76f2a3023af
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-03-01 03:20:39 +0100

    order cancel reject

--- x/fix/client/cli/tx_order_cancel_reject.go
+++ x/fix/client/cli/tx_order_cancel_reject.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/jim380/Re/x/fix/types"
+	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
 )
 
@@ -14,13 +15,30 @@ var _ = strconv.Itoa(0)
 
 func CmdOrderCancelReject() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "order-cancel-reject [session-id]",
+		Use:   "order-cancel-reject [sessionID] [OrderID] [OrigClOrdID] [ClOrdID] [CxlRejReason] [CxlRejResponseTo]",
 		Short: "Broadcast message order-cancel-reject",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(6),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			argSessionID := args[0]
 
 			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			argSessionID := args[0]
+
+			argOrderID := args[1]
+
+			argOrigClOrdID := args[2]
+
+			argClOrdID := args[3]
+
+			argCxlRejReason, err := cast.ToInt64E(args[4])
+			if err != nil {
+				return err
+			}
+
+			argCxlRejResponseTo, err := cast.ToInt64E(args[5])
 			if err != nil {
 				return err
 			}
@@ -28,6 +46,11 @@ func CmdOrderCancelReject() *cobra.Command {
 			msg := types.NewMsgOrderCancelReject(
 				clientCtx.GetFromAddress().String(),
 				argSessionID,
+				argOrderID,
+				argOrigClOrdID,
+				argClOrdID,
+				argCxlRejReason,
+				argCxlRejResponseTo,
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
