ec67e51a79cc15e3b82eb05d866958f5d1e73834
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-03-17 11:28:59 +0100

    refactor keeper file structure

--- x/fix/keeper/msg_server_logout.go
+++ x/fix/keeper/msg_server_logout.go
@@ -9,6 +9,38 @@ import (
 	"github.com/jim380/Re/x/fix/types"
 )
 
+// LogoutInitiator requests for logout from the session
+func (k msgServer) LogoutInitiator(goCtx context.Context, msg *types.MsgLogoutInitiator) (*types.MsgLogoutInitiatorResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	//check for if this session Name exists already
+	session, found := k.GetSessions(ctx, msg.SessionID)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrEmptySession, "Session Name: %s", msg.SessionID)
+	}
+
+	//TODO
+	//include all cheecks
+	//check that the address initiating a session logout belongs to the session
+
+	sessionLogoutInitiator := types.SessionLogout{
+		InitiatorAddress:       msg.InitiatorAddress,
+		SessionID:              msg.SessionID,
+		SessionLogoutInitiator: msg.SessionLogoutInitiator,
+	}
+
+	sessionLogoutInitiator.InitiatorAddress = session.InitiatorAddress
+	sessionLogoutInitiator.SessionLogoutInitiator.Header = session.LogonInitiator.Header
+	sessionLogoutInitiator.SessionLogoutInitiator.Trailer = session.LogonInitiator.Trailer
+	sessionLogoutInitiator.SessionLogoutInitiator.Header.MsgType = "5"
+	sessionLogoutInitiator.SessionLogoutInitiator.Text = msg.SessionLogoutInitiator.Text
+
+	//set session logout to store
+	k.SetSessionLogout(ctx, msg.SessionID, sessionLogoutInitiator)
+
+	return &types.MsgLogoutInitiatorResponse{}, nil
+}
+
 // LogoutAcceptor accepts to logout from the session
 func (k msgServer) LogoutAcceptor(goCtx context.Context, msg *types.MsgLogoutAcceptor) (*types.MsgLogoutAcceptorResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
--- x/fix/keeper/query_sessions.go
+++ x/fix/keeper/query_sessions.go
@@ -53,3 +53,89 @@ func (k Keeper) Sessions(goCtx context.Context, req *types.QueryGetSessionsReque
 
 	return &types.QueryGetSessionsResponse{Sessions: sessions}, nil
 }
+
+
+func (k Keeper) SessionRejectAll(goCtx context.Context, req *types.QueryAllSessionRejectRequest) (*types.QueryAllSessionRejectResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	var sessionRejects []types.SessionReject
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	store := ctx.KVStore(k.storeKey)
+	sessionRejectStore := prefix.NewStore(store, types.GetSessionRejectKey())
+
+	pageRes, err := query.Paginate(sessionRejectStore, req.Pagination, func(key []byte, value []byte) error {
+		var sessionReject types.SessionReject
+		if err := k.cdc.Unmarshal(value, &sessionReject); err != nil {
+			return err
+		}
+
+		sessionRejects = append(sessionRejects, sessionReject)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAllSessionRejectResponse{SessionReject: sessionRejects, Pagination: pageRes}, nil
+}
+
+func (k Keeper) SessionReject(goCtx context.Context, req *types.QueryGetSessionRejectRequest) (*types.QueryGetSessionRejectResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	sessionReject, found := k.GetSessionReject(ctx, req.SessionID)
+	if !found {
+		return nil, sdkerrors.ErrKeyNotFound
+	}
+
+	return &types.QueryGetSessionRejectResponse{SessionReject: sessionReject}, nil
+}
+
+func (k Keeper) SessionLogoutAll(goCtx context.Context, req *types.QueryAllSessionLogoutRequest) (*types.QueryAllSessionLogoutResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	var sessionLogouts []types.SessionLogout
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	store := ctx.KVStore(k.storeKey)
+	sessionLogoutStore := prefix.NewStore(store, types.GetSessionLogoutKey())
+
+	pageRes, err := query.Paginate(sessionLogoutStore, req.Pagination, func(key []byte, value []byte) error {
+		var sessionLogout types.SessionLogout
+		if err := k.cdc.Unmarshal(value, &sessionLogout); err != nil {
+			return err
+		}
+
+		sessionLogouts = append(sessionLogouts, sessionLogout)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAllSessionLogoutResponse{SessionLogout: sessionLogouts, Pagination: pageRes}, nil
+}
+
+func (k Keeper) SessionLogout(goCtx context.Context, req *types.QueryGetSessionLogoutRequest) (*types.QueryGetSessionLogoutResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	sessionLogout, found := k.GetSessionLogout(ctx, req.SessionID)
+	if !found {
+		return nil, sdkerrors.ErrKeyNotFound
+	}
+
+	return &types.QueryGetSessionLogoutResponse{SessionLogout: sessionLogout}, nil
+}
+
