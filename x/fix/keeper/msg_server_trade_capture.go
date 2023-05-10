3a2ae3ad57ecc6f24341959f3642ed3ec634da3c
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-05-10 01:32:26 +0100

    added more checks and refactored the errors code

--- x/fix/keeper/msg_server_trade_capture.go
+++ x/fix/keeper/msg_server_trade_capture.go
@@ -36,23 +36,59 @@ func (k msgServer) TradeCaptureReport(goCtx context.Context, msg *types.MsgTrade
 	// get Order Execution Report
 	ordersExecutionReport, found := k.GetOrdersExecutionReport(ctx, msg.OrderID)
 	if !found {
-		return nil, sdkerrors.Wrapf(types.ErrOrderIsNotFound, "Order Execution Report: %s", msg.OrderID)
+		return nil, sdkerrors.Wrapf(types.ErrOrdersExecutionReportIsNotFound, "Order Execution Report: %s", msg.OrderID)
 	}
 
 	// check that the owner of the order execution report(the executing party) matches the creator of trade capture report
 	if ordersExecutionReport.Creator != msg.Creator {
-		// handle err
+		return nil, sdkerrors.Wrapf(types.ErrNotAccountCreator, "Order Execution Report Creator: %s", msg.Creator)
 	}
 
 	// check that these fields from order execution report match the provided values
 	if ordersExecutionReport.OrderID != msg.OrderID {
-		// handle err
+		return nil, sdkerrors.Wrapf(types.ErrOrderIDIsNotFound, "OrderID: %s", msg.OrderID)
 	}
 	if ordersExecutionReport.ExecID != msg.ExecID {
-		// handle err
+		return nil, sdkerrors.Wrapf(types.ErrExecIDIsNotFound, "ExecID: %s", msg.ExecID)
 	}
 
-	
+	// check that mandatory Trade Capture Report fields are not empty
+	if msg.TradeReportID == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeReportIDIsNotFound, "TradeReportID: %s", msg.TradeReportID)
+	}
+	if msg.TradeReportTransType == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeReportTransTypeIsNotFound, "TradeReportTransType: %s", msg.TradeReportTransType)
+	}
+	if msg.TrdType == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTrdTypeIsNotFound, "TrdType: %s", msg.TrdType)
+	}
+	if msg.Side == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureSideIsNotFound, "Side: %s", msg.Side)
+	}
+	if msg.OrderQty == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureOrderQtyIsNotFound, "OrderQty: %s", msg.OrderQty)
+	}
+	if msg.LastQty == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureLastQtyIsNotFound, "LastQty: %s", msg.LastQty)
+	}
+	if msg.LastPx == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureLastPxIsNotFound, "LastPx: %s", msg.LastPx)
+	}
+	if msg.GrossTradeAmt == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureGrossTradeAmtIsNotFound, "GrossTradeAmt: %s", msg.GrossTradeAmt)
+	}
+	if msg.Symbol == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureSymbolIsNotFound, "Symbol: %s", msg.Symbol)
+	}
+	if msg.SecurityID == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureSecurityIDIsNotFound, "SecurityID: %s", msg.SecurityID)
+	}
+	if msg.TradeDate == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureTradeDateIsNotFound, "TradeDate: %s", msg.TradeDate)
+	}
+	if msg.TransactTime == "" {
+		return nil, sdkerrors.Wrapf(types.ErrTradeCaptureTransactTimeIsNotFound, "TransactTime: %s", msg.TransactTime)
+	}
 
 	return &types.MsgTradeCaptureReportResponse{}, nil
 }
--- x/fix/types/errors.go
+++ x/fix/types/errors.go
@@ -6,64 +6,159 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+// Error Code Enums
+
+const (
+	// DID
+	errInvalidDidDocument uint32 = iota + 101
+	errAccountIsTaken
+	errNotDIDCreator
+	errDIDIsTaken
+	errCompanyNameIsTaken
+	errWebsiteIstaken
+	errNotAccountCreator
+
+	// session
+	errSessionNameFound
+	errSessionSameDID
+	errEmptySession
+	errWrongSession
+	errIncorrectDID
+	errSessionIsAccepted
+
+	// Quote
+	errQuoteIsEmpty
+	errQuoteSession
+	errQuoteReqIDIsTaken
+	errQuoteReqIDIsEmpty
+	errQuoteSymbolIsEmpty
+	errQuoteSideIsEmpty
+	errQuoteBidPxIsEmpty
+	errQuoteCurrencyIsEmpty
+	errQuoteTypeIsEmpty
+	errMICInQuoteRquestIsNotFound
+	errQuoteRequestIsRejected
+	errQuoteAcknowledgementCreatorIsWrong
+	errWrongSessionIDInQuote
+	errQuoteRequestRejectCreatorIsWrong
+	errQuoteRequestIsAcknowledged
+	errQuoteRequestRejectReasonIsEmpty
+
+	// Market Data
+	errSubscriptionRequestTypeIsEmpty
+	errMarketDepthIsEmpty
+	errMdUpdateTypeIsEmpty
+	errNoRelatedSymIsEmpty
+	errSymbolIsEmpty
+	errMdReqIDIsNotFound
+	errMarketDataSnapShotFullRefreshCreatorIsWrong
+	errMarketDataRequestIsRejected
+	errMdReqIDIsEmpty
+	errNoMDEntriesIsEmpty
+	errMdUpdateActionIsEmpty
+	errMdEntryTypeIsEmpty
+	errMdEntryPxIsEmpty
+	errMdEntrySizeIsEmpty
+	errMarketDataSession
+	errWrongSessionIDInMarketData
+	errMarketDataRequestRejectCreatorIsWrong
+	errMarketDataRequestIsAcknowlodged
+	errMdReqRejReasonIsEmpty
+
+	// Orders
+	errOrdersExecutionReportIsNotFound
+	errOrderIDIsNotFound
+	errExecIDIsNotFound
+
+	// Trade Capture
+	errTradeReportIDIsNotFound
+	errTradeReportTransTypeIsNotFound
+	errTrdTypeIsNotFound
+	errTradeCaptureSideIsNotFound
+	errTradeCaptureOrderQtyIsNotFound
+	errTradeCaptureLastQtyIsNotFound
+	errTradeCaptureLastPxIsNotFound
+	errTradeCaptureGrossTradeAmtIsNotFound
+	errTradeCaptureSymbolIsNotFound
+	errTradeCaptureSecurityIDIsNotFound
+	errTradeCaptureTradeDateIsNotFound
+	errTradeCaptureTransactTimeIsNotFound
+)
+
 // x/fix module sentinel errors
 var (
 	// DID
-	ErrInvalidDidDocument = sdkerrors.Register(ModuleName, 2, "Invalid DID Document")
-	ErrAccountIsTaken     = sdkerrors.Register(ModuleName, 3, "Account Address is used already")
-	ErrNotDIDCreator      = sdkerrors.Register(ModuleName, 4, "Account creator must be the same with DID creator")
-	ErrDIDIsTaken         = sdkerrors.Register(ModuleName, 5, "DID exists with an account already")
-	ErrCompanyNameIsTaken = sdkerrors.Register(ModuleName, 6, "Company Name exists with an account already")
-	ErrWebsiteIstaken     = sdkerrors.Register(ModuleName, 7, "Website is used already")
-	ErrNotAccountCreator  = sdkerrors.Register(ModuleName, 8, "Incorrect Account Owner")
+	ErrInvalidDidDocument = sdkerrors.Register(ModuleName, errInvalidDidDocument, "Invalid DID Document")
+	ErrAccountIsTaken     = sdkerrors.Register(ModuleName, errAccountIsTaken, "Account Address is used already")
+	ErrNotDIDCreator      = sdkerrors.Register(ModuleName, errNotDIDCreator, "Account creator must be the same with DID creator")
+	ErrDIDIsTaken         = sdkerrors.Register(ModuleName, errDIDIsTaken, "DID exists with an account already")
+	ErrCompanyNameIsTaken = sdkerrors.Register(ModuleName, errCompanyNameIsTaken, "Company Name exists with an account already")
+	ErrWebsiteIstaken     = sdkerrors.Register(ModuleName, errWebsiteIstaken, "Website is used already")
+	ErrNotAccountCreator  = sdkerrors.Register(ModuleName, errNotAccountCreator, "Incorrect Account Owner")
 
 	// Session
-	ErrSessionNameFound  = sdkerrors.Register(ModuleName, 9, "Session Name exists")
-	ErrSessionSameDID    = sdkerrors.Register(ModuleName, 10, "Session can not use same DID for senderCompID and targetCompID")
-	ErrEmptySession      = sdkerrors.Register(ModuleName, 11, "Session does not Exist")
-	ErrWrongSession      = sdkerrors.Register(ModuleName, 12, "The Session provided does not tally with account")
-	ErrIncorrectDID      = sdkerrors.Register(ModuleName, 13, "senderCompID and targetCompID does not match in session")
-	ErrSessionIsAccepted = sdkerrors.Register(ModuleName, 14, "session accepted already")
+	ErrSessionNameFound  = sdkerrors.Register(ModuleName, errSessionNameFound, "Session Name exists")
+	ErrSessionSameDID    = sdkerrors.Register(ModuleName, errSessionSameDID, "Session can not use same DID for senderCompID and targetCompID")
+	ErrEmptySession      = sdkerrors.Register(ModuleName, errEmptySession, "Session does not Exist")
+	ErrWrongSession      = sdkerrors.Register(ModuleName, errWrongSession, "The Session provided does not tally with account")
+	ErrIncorrectDID      = sdkerrors.Register(ModuleName, errIncorrectDID, "senderCompID and targetCompID does not match in session")
+	ErrSessionIsAccepted = sdkerrors.Register(ModuleName, errSessionIsAccepted, "session accepted already")
 
 	// Quote
-	ErrQuoteIsEmpty                       = sdkerrors.Register(ModuleName, 15, "Quote Is Empty")
-	ErrQuoteSession                       = sdkerrors.Register(ModuleName, 16, "No established FIX session between parties")
-	ErrQuoteReqIDIsTaken                  = sdkerrors.Register(ModuleName, 17, "QuoteReqID exists already for a given Quote Request")
-	ErrQuoteReqIDIsEmpty                  = sdkerrors.Register(ModuleName, 18, "QuoteReqID is empty")
-	ErrQuoteSymbolIsEmpty                 = sdkerrors.Register(ModuleName, 19, "Quote Symbol is empty")
-	ErrQuoteSideIsEmpty                   = sdkerrors.Register(ModuleName, 20, "Quote Side is empty")
-	ErrQuoteBidPxIsEmpty                  = sdkerrors.Register(ModuleName, 21, "Quote BidPx is empty")
-	ErrQuoteCurrencyIsEmpty               = sdkerrors.Register(ModuleName, 22, "Quote Currency is empty")
-	ErrQuoteTypeIsEmpty                   = sdkerrors.Register(ModuleName, 23, "QuoteType is empty")
-	ErrMICInQuoteRquestIsNotFound         = sdkerrors.Register(ModuleName, 24, "MIC used in Quote is not registered")
-	ErrQuoteRequestIsRejected             = sdkerrors.Register(ModuleName, 25, "Quote Request has been Rejected")
-	ErrQuoteAcknowledgementCreatorIsWrong = sdkerrors.Register(ModuleName, 26, "This Account Address is not allowed to Acknowledge this Quote Request")
-	ErrWrongSessionIDInQuote              = sdkerrors.Register(ModuleName, 27, "This Session ID does not tally with the Quote Request")
-	ErrQuoteRequestRejectCreatorIsWrong   = sdkerrors.Register(ModuleName, 28, "This Account Address is not allowed to Reject this Quote Request")
-	ErrQuoteRequestIsAcknowledged         = sdkerrors.Register(ModuleName, 29, "Quote Request has been Acknowledged")
-	ErrQuoteRequestRejectReasonIsEmpty    = sdkerrors.Register(ModuleName, 30, "Quote Request Reject Reason is empty")
+	ErrQuoteIsEmpty                       = sdkerrors.Register(ModuleName, errQuoteIsEmpty, "Quote Is Empty")
+	ErrQuoteSession                       = sdkerrors.Register(ModuleName, errQuoteSession, "No established FIX session between parties")
+	ErrQuoteReqIDIsTaken                  = sdkerrors.Register(ModuleName, errQuoteReqIDIsTaken, "QuoteReqID exists already for a given Quote Request")
+	ErrQuoteReqIDIsEmpty                  = sdkerrors.Register(ModuleName, errQuoteReqIDIsEmpty, "QuoteReqID is empty")
+	ErrQuoteSymbolIsEmpty                 = sdkerrors.Register(ModuleName, errQuoteSymbolIsEmpty, "Quote Symbol is empty")
+	ErrQuoteSideIsEmpty                   = sdkerrors.Register(ModuleName, errQuoteSideIsEmpty, "Quote Side is empty")
+	ErrQuoteBidPxIsEmpty                  = sdkerrors.Register(ModuleName, errQuoteBidPxIsEmpty, "Quote BidPx is empty")
+	ErrQuoteCurrencyIsEmpty               = sdkerrors.Register(ModuleName, errQuoteCurrencyIsEmpty, "Quote Currency is empty")
+	ErrQuoteTypeIsEmpty                   = sdkerrors.Register(ModuleName, errQuoteTypeIsEmpty, "QuoteType is empty")
+	ErrMICInQuoteRquestIsNotFound         = sdkerrors.Register(ModuleName, errMICInQuoteRquestIsNotFound, "MIC used in Quote is not registered")
+	ErrQuoteRequestIsRejected             = sdkerrors.Register(ModuleName, errQuoteRequestIsRejected, "Quote Request has been Rejected")
+	ErrQuoteAcknowledgementCreatorIsWrong = sdkerrors.Register(ModuleName, errQuoteAcknowledgementCreatorIsWrong, "This Account Address is not allowed to Acknowledge this Quote Request")
+	ErrWrongSessionIDInQuote              = sdkerrors.Register(ModuleName, errWrongSessionIDInQuote, "This Session ID does not tally with the Quote Request")
+	ErrQuoteRequestRejectCreatorIsWrong   = sdkerrors.Register(ModuleName, errQuoteRequestRejectCreatorIsWrong, "This Account Address is not allowed to Reject this Quote Request")
+	ErrQuoteRequestIsAcknowledged         = sdkerrors.Register(ModuleName, errQuoteRequestIsAcknowledged, "Quote Request has been Acknowledged")
+	ErrQuoteRequestRejectReasonIsEmpty    = sdkerrors.Register(ModuleName, errQuoteRequestRejectReasonIsEmpty, "Quote Request Reject Reason is empty")
 
 	// Market Data
-	ErrSubscriptionRequestTypeIsEmpty              = sdkerrors.Register(ModuleName, 31, "SubscriptionRequestType is empty")
-	ErrMarketDepthIsEmpty                          = sdkerrors.Register(ModuleName, 32, "MarketDepth is empty")
-	ErrMdUpdateTypeIsEmpty                         = sdkerrors.Register(ModuleName, 33, "MdUpdateType is empty")
-	ErrNoRelatedSymIsEmpty                         = sdkerrors.Register(ModuleName, 34, "NoRelatedSym is empty")
-	ErrSymbolIsEmpty                               = sdkerrors.Register(ModuleName, 35, "Symbol is empty")
-	ErrMdReqIDIsNotFound                           = sdkerrors.Register(ModuleName, 36, "MdReqID is not found")
-	ErrMarketDataSnapShotFullRefreshCreatorIsWrong = sdkerrors.Register(ModuleName, 37, "This account address is not allowed to respond to the market data request")
-	ErrMarketDataRequestIsRejected                 = sdkerrors.Register(ModuleName, 38, "Market Data Request has been rejected")
-	ErrMdReqIDIsEmpty                              = sdkerrors.Register(ModuleName, 39, "MdReqID is empty")
-	ErrNoMDEntriesIsEmpty                          = sdkerrors.Register(ModuleName, 40, "NoMDEntries is empty")
-	ErrMdUpdateActionIsEmpty                       = sdkerrors.Register(ModuleName, 41, "MdUpdateAction is empty")
-	ErrMdEntryTypeIsEmpty                          = sdkerrors.Register(ModuleName, 42, "MdEntryType is empty")
-	ErrMdEntryPxIsEmpty                            = sdkerrors.Register(ModuleName, 43, "MdEntryPx is empty")
-	ErrMdEntrySizeIsEmpty                          = sdkerrors.Register(ModuleName, 44, "MdEntrySize is empty")
-	ErrMarketDataSession                           = sdkerrors.Register(ModuleName, 45, "No established FIX session between parties")
-	ErrWrongSessionIDInMarketData                  = sdkerrors.Register(ModuleName, 46, "This Session ID does not tally with the Market Data Request")
-	ErrMarketDataRequestRejectCreatorIsWrong       = sdkerrors.Register(ModuleName, 47, "This account address is not allowed to respond to the market data request reject")
-	ErrMarketDataRequestIsAcknowlodged             = sdkerrors.Register(ModuleName, 48, "Market Data Request has been acknowledged")
-	ErrMdReqRejReasonIsEmpty                       = sdkerrors.Register(ModuleName, 49, "MdReqRejReason is empty")
+	ErrSubscriptionRequestTypeIsEmpty              = sdkerrors.Register(ModuleName, errSubscriptionRequestTypeIsEmpty, "SubscriptionRequestType is empty")
+	ErrMarketDepthIsEmpty                          = sdkerrors.Register(ModuleName, errMarketDepthIsEmpty, "MarketDepth is empty")
+	ErrMdUpdateTypeIsEmpty                         = sdkerrors.Register(ModuleName, errMdUpdateTypeIsEmpty, "MdUpdateType is empty")
+	ErrNoRelatedSymIsEmpty                         = sdkerrors.Register(ModuleName, errNoRelatedSymIsEmpty, "NoRelatedSym is empty")
+	ErrSymbolIsEmpty                               = sdkerrors.Register(ModuleName, errSymbolIsEmpty, "Symbol is empty")
+	ErrMdReqIDIsNotFound                           = sdkerrors.Register(ModuleName, errMdReqIDIsNotFound, "MdReqID is not found")
+	ErrMarketDataSnapShotFullRefreshCreatorIsWrong = sdkerrors.Register(ModuleName, errMarketDataSnapShotFullRefreshCreatorIsWrong, "This account address is not allowed to respond to the market data request")
+	ErrMarketDataRequestIsRejected                 = sdkerrors.Register(ModuleName, errMarketDataRequestIsRejected, "Market Data Request has been rejected")
+	ErrMdReqIDIsEmpty                              = sdkerrors.Register(ModuleName, errMdReqIDIsEmpty, "MdReqID is empty")
+	ErrNoMDEntriesIsEmpty                          = sdkerrors.Register(ModuleName, errNoMDEntriesIsEmpty, "NoMDEntries is empty")
+	ErrMdUpdateActionIsEmpty                       = sdkerrors.Register(ModuleName, errMdUpdateActionIsEmpty, "MdUpdateAction is empty")
+	ErrMdEntryTypeIsEmpty                          = sdkerrors.Register(ModuleName, errMdEntryTypeIsEmpty, "MdEntryType is empty")
+	ErrMdEntryPxIsEmpty                            = sdkerrors.Register(ModuleName, errMdEntryPxIsEmpty, "MdEntryPx is empty")
+	ErrMdEntrySizeIsEmpty                          = sdkerrors.Register(ModuleName, errMdEntrySizeIsEmpty, "MdEntrySize is empty")
+	ErrMarketDataSession                           = sdkerrors.Register(ModuleName, errMarketDataSession, "No established FIX session between parties")
+	ErrWrongSessionIDInMarketData                  = sdkerrors.Register(ModuleName, errWrongSessionIDInMarketData, "This Session ID does not tally with the Market Data Request")
+	ErrMarketDataRequestRejectCreatorIsWrong       = sdkerrors.Register(ModuleName, errMarketDataRequestRejectCreatorIsWrong, "This account address is not allowed to respond to the market data request reject")
+	ErrMarketDataRequestIsAcknowlodged             = sdkerrors.Register(ModuleName, errMarketDataRequestIsAcknowlodged, "Market Data Request has been acknowledged")
+	ErrMdReqRejReasonIsEmpty                       = sdkerrors.Register(ModuleName, errMdReqRejReasonIsEmpty, "MdReqRejReason is empty")
+
+	// Orders
+	ErrOrdersExecutionReportIsNotFound = sdkerrors.Register(ModuleName, errOrdersExecutionReportIsNotFound, "Order Execution Report is not found")
+	ErrOrderIDIsNotFound               = sdkerrors.Register(ModuleName, errOrderIDIsNotFound, "OrderID is not found")
+	ErrExecIDIsNotFound                = sdkerrors.Register(ModuleName, errExecIDIsNotFound, "ExecID is not found")
 
 	// Trade Capture
-	ErrOrderIsNotFound = sdkerrors.Register(ModuleName, 50, "Order is not found")
+	ErrTradeReportIDIsNotFound             = sdkerrors.Register(ModuleName, errTradeReportIDIsNotFound, "TradeReportID is not found")
+	ErrTradeReportTransTypeIsNotFound      = sdkerrors.Register(ModuleName, errTradeReportTransTypeIsNotFound, "TradeReportTransType is not found")
+	ErrTrdTypeIsNotFound                   = sdkerrors.Register(ModuleName, errTrdTypeIsNotFound, "TrdType is not found")
+	ErrTradeCaptureSideIsNotFound          = sdkerrors.Register(ModuleName, errTradeCaptureSideIsNotFound, "Trade Capture Side is not found")
+	ErrTradeCaptureOrderQtyIsNotFound      = sdkerrors.Register(ModuleName, errTradeCaptureOrderQtyIsNotFound, "Trade Capture OrderQty is not found")
+	ErrTradeCaptureLastQtyIsNotFound       = sdkerrors.Register(ModuleName, errTradeCaptureLastQtyIsNotFound, "Trade Capture LastQty is not found")
+	ErrTradeCaptureLastPxIsNotFound        = sdkerrors.Register(ModuleName, errTradeCaptureLastPxIsNotFound, "Trade Capture LastPx is not found")
+	ErrTradeCaptureGrossTradeAmtIsNotFound = sdkerrors.Register(ModuleName, errTradeCaptureGrossTradeAmtIsNotFound, "Trade Capture GrossTradeAmt is not found")
+	ErrTradeCaptureSymbolIsNotFound        = sdkerrors.Register(ModuleName, errTradeCaptureSymbolIsNotFound, "Trade Capture Symbol is not found")
+	ErrTradeCaptureSecurityIDIsNotFound    = sdkerrors.Register(ModuleName, errTradeCaptureSecurityIDIsNotFound, "Trade Capture SecurityID is not found")
+	ErrTradeCaptureTradeDateIsNotFound     = sdkerrors.Register(ModuleName, errTradeCaptureTradeDateIsNotFound, "Trade Capture TradeDate is not found")
+	ErrTradeCaptureTransactTimeIsNotFound  = sdkerrors.Register(ModuleName, errTradeCaptureTransactTimeIsNotFound, "Trade Capture TransactTime is not found")
 )
