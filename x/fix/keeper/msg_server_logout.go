ae2ab0329e31b921333803910e6ba40ad6aebd6c
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-06-26 03:43:57 +0100

    fix errors on fix logout

--- x/fix/keeper/msg_server_logout.go
+++ x/fix/keeper/msg_server_logout.go
@@ -2,10 +2,10 @@ package keeper
 
 import (
 	"context"
-	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/jim380/Re/utils/constants"
 	"github.com/jim380/Re/x/fix/types"
 )
 
@@ -19,16 +19,27 @@ func (k msgServer) LogoutInitiator(goCtx context.Context, msg *types.MsgLogoutIn
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.InitiatorAddress)
 	}
 
-	// check for if this session Name exists already
+	// check for if this sessionID exists already
 	session, found := k.GetSessions(ctx, msg.SessionID)
 	if !found {
 		return nil, sdkerrors.Wrapf(types.ErrEmptySession, "Session Name: %s", msg.SessionID)
 	}
 
-	// TODO
-	// include all cheecks
-	// check that the address initiating a session logout belongs to the session
+	// check that logon is established between both parties and that logon status equals to "loggedIn"
+	if session.Status != constants.LoggedInStatus {
+		return nil, sdkerrors.Wrapf(types.ErrSessionIsNotLoggedIn, "Status of Session: %s", session.Status)
+	}
 
+	// check that the address initiating a session logout belongs to the session
+	if session.LogonInitiator.Header.SenderCompID != msg.InitiatorAddress && session.LogonAcceptor.Header.SenderCompID != msg.InitiatorAddress {
+		return nil, sdkerrors.Wrapf(types.ErrNotAccountCreator, "Logout Initiator: %s", msg.InitiatorAddress)
+	}
+
+	// check for if this sessionLogout exists already
+	initiatedLogoutSesssion, found := k.GetSessionLogout(ctx, msg.SessionID)
+	if found {
+		return nil, sdkerrors.Wrapf(types.ErrSessionLogoutIsInitiated, "Session Logout: %s", initiatedLogoutSesssion.SessionID)
+	}
 	sessionLogoutInitiator := types.SessionLogout{
 		SessionID:              msg.SessionID,
 		SessionLogoutInitiator: msg.SessionLogoutInitiator,
@@ -62,22 +73,21 @@ func (k msgServer) LogoutAcceptor(goCtx context.Context, msg *types.MsgLogoutAcc
 		return nil, sdkerrors.Wrapf(types.ErrEmptySession, "Session Name: %s", msg.SessionID)
 	}
 
+	// check that the address accepting the logout participated in the session
+	if session.LogonInitiator.Header.SenderCompID != msg.AcceptorAddress && session.LogonAcceptor.Header.SenderCompID != msg.AcceptorAddress {
+		return nil, sdkerrors.Wrapf(types.ErrNotAccountCreator, "Logout Acceptor: %s", msg.AcceptorAddress)
+	}
+
 	// get logout session, if not found, then logout is not initiated
 	initiatedLogoutSesssion, found := k.GetSessionLogout(ctx, msg.SessionID)
 	if !found {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %s Logout was not initiated", msg.SessionID))
+		return nil, sdkerrors.Wrapf(types.ErrSessionLogoutIsNotInitiated, "Session Logout: %s", initiatedLogoutSesssion.SessionID)
 	}
 
-	/*
-		if initiatedLogoutSesssion.InitiatorAddress == msg.AcceptorAddress {
-			return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %s Wrong account address", msg.AcceptorAddress))
-		}
-	*/
-
-	// TODO
-	// Include checks
-	// when logout is accpted, session name should be removed from the store
-	// check that the address accepting the logout participated in the session
+	// check that the session logout has not been accepted
+	if initiatedLogoutSesssion.SessionLogoutAcceptor != nil {
+		return nil, sdkerrors.Wrapf(types.ErrSessionLogoutIsAccepted, "Session Logout: %s", initiatedLogoutSesssion.SessionLogoutAcceptor)
+	}
 
 	sessionLogoutAcceptor := types.SessionLogout{
 		SessionID:              msg.SessionID,
@@ -92,6 +102,7 @@ func (k msgServer) LogoutAcceptor(goCtx context.Context, msg *types.MsgLogoutAcc
 	sessionLogoutAcceptor.SessionLogoutAcceptor.Text = msg.SessionLogoutAcceptor.Text
 	sessionLogoutAcceptor.SessionLogoutAcceptor.Header.MsgType = "5"
 
+	// set session logout acceptor to store
 	k.SetSessionLogout(ctx, msg.SessionID, sessionLogoutAcceptor)
 
 	// remove session from store
