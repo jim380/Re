8414468066982a47e76d5346d96c1c16392bec89
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-06-30 13:47:31 +0100

    added TestLogonReject

--- x/fix/keeper/msg_server_logon_test.go
+++ x/fix/keeper/msg_server_logon_test.go
@@ -489,3 +489,286 @@ func (suite *KeeperTestSuite) TestLogonAcceptor() {
 		})
 	}
 }
+
+func (suite *KeeperTestSuite) TestLogonReject() {
+	type args struct {
+		session        types.Sessions
+		msgLogonReject types.MsgLogonReject
+	}
+
+	type errArgs struct {
+		shouldPass bool
+		contains   string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		errArgs errArgs
+	}{
+		{
+			"Logon Request can be Rejected when the request has not been accepted or rejected",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.LogonRequestStatus,
+					IsAccepted:    false,
+				},
+				msgLogonReject: types.MsgLogonReject{
+					AcceptorAddress: suite.address[1].String(),
+					SessionID:       "sessionID1",
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[0].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					Text: "Not Ready",
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				},
+			},
+			errArgs{
+				shouldPass: true,
+				contains:   "",
+			},
+		},
+		{
+			"SessionID in Logon Request does not match the sessionID provided by Acceptor",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1xyz",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.LogonRequestStatus,
+					IsAccepted:    false,
+				},
+				msgLogonReject: types.MsgLogonReject{
+					AcceptorAddress: suite.address[1].String(),
+					SessionID:       "sessionID1",
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[0].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					Text: "Not Ready",
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				},
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"Wrong Account Address",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.LogonRequestStatus,
+					IsAccepted:    false,
+				},
+				msgLogonReject: types.MsgLogonReject{
+					AcceptorAddress: suite.address[0].String(),
+					SessionID:       "sessionID1",
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[0].String(),
+						TargetCompID: suite.address[1].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					Text: "Not Ready",
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				},
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"Logon Request has been accepted",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.LoggedInStatus,
+					IsAccepted:    true,
+				},
+				msgLogonReject: types.MsgLogonReject{
+					AcceptorAddress: suite.address[1].String(),
+					SessionID:       "sessionID1",
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[0].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					Text: "Not Ready",
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				},
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"Logon Request has been Rejected",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.RejectedStatus,
+					IsAccepted:    false,
+				},
+				msgLogonReject: types.MsgLogonReject{
+					AcceptorAddress: suite.address[1].String(),
+					SessionID:       "sessionID1",
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[0].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					Text: "Not Ready",
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				},
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+
+			// set session
+			suite.fixKeeper.SetSessions(suite.ctx, tc.args.session.SessionID, tc.args.session)
+
+			// call LogonReject method
+			res, err := suite.msgServer.LogonReject(sdk.WrapSDKContext(suite.ctx), &tc.args.msgLogonReject)
+			_, found := suite.fixKeeper.GetSessions(suite.ctx, tc.args.msgLogonReject.SessionID)
+
+			if tc.errArgs.shouldPass {
+				suite.Require().True(found)
+				suite.Require().NoError(err, tc.name)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err, tc.name)
+				suite.Require().Nil(res)
+				suite.Require().True(strings.Contains(err.Error(), tc.errArgs.contains))
+			}
+
+		})
+	}
+}
