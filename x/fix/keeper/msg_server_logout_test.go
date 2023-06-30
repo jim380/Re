1525ccf7053d7e2c7748644871ea05b360ee40f7
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-06-30 16:08:11 +0100

    fix TestLogoutInitiator() logic

--- x/fix/keeper/msg_server_logout_test.go
+++ x/fix/keeper/msg_server_logout_test.go
@@ -1,7 +1,11 @@
 package keeper_test
 
 import (
+	"strings"
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/jim380/Re/utils/constants"
 	"github.com/jim380/Re/x/fix/types"
 )
 
@@ -24,11 +28,141 @@ func (suite *KeeperTestSuite) TestLogoutInitiator() {
 		{
 			"Valid Logout Request",
 			args{
-				session:            types.Sessions{},
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
 				msgLogoutInitiator: *types.NewMsgLogoutInitiator(suite.address[0].String(), "sessionID", types.SessionLogoutInitiator{Text: "Request to Logout from the current session"}),
 			},
 			errArgs{
 				shouldPass: true,
+				contains:   "",
+			},
+		},
+		{
+			"Logout Request Initiator's Account Address is not found the session",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
+				msgLogoutInitiator: *types.NewMsgLogoutInitiator(suite.address[2].String(), "sessionID", types.SessionLogoutInitiator{Text: "Request to Logout from the current session"}),
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"SessionID does not exist",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
+				msgLogoutInitiator: *types.NewMsgLogoutInitiator(suite.address[0].String(), "sessionIDxyz", types.SessionLogoutInitiator{Text: "Request to Logout from the current session"}),
+			},
+			errArgs{
+				shouldPass: false,
 				contains:   "",
 			},
 		},
@@ -38,9 +172,22 @@ func (suite *KeeperTestSuite) TestLogoutInitiator() {
 		suite.Run(tc.name, func() {
 			suite.SetupTest()
 
+			// set session
+			suite.fixKeeper.SetSessions(suite.ctx, tc.args.session.SessionID, tc.args.session)
+
 			// call LogoutInitiator method
 			res, err := suite.msgServer.LogoutInitiator(sdk.WrapSDKContext(suite.ctx), &tc.args.msgLogoutInitiator)
-			session, found := suite.fixKeeper.GetSessions(suite.ctx, tc.args.msgLogoutInitiator.SessionID)
+			_, found := suite.fixKeeper.GetSessions(suite.ctx, tc.args.msgLogoutInitiator.SessionID)
+
+			if tc.errArgs.shouldPass {
+				suite.Require().True(found)
+				suite.Require().NoError(err, tc.name)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err, tc.name)
+				suite.Require().Nil(res)
+				suite.Require().True(strings.Contains(err.Error(), tc.errArgs.contains))
+			}
 
 		})
 	}
