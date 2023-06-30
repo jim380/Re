8346f80909c96514b6ef419f02cd79b1f232da58
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-06-30 12:18:15 +0100

    added TestLogonAcceptor

--- x/fix/keeper/msg_server_logon_test.go
+++ x/fix/keeper/msg_server_logon_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/jim380/Re/utils/constants"
 	"github.com/jim380/Re/x/fix/types"
 )
 
@@ -165,6 +166,323 @@ func (suite *KeeperTestSuite) TestLogonInitiator() {
 				suite.Require().Error(err, tc.name)
 				suite.Require().Nil(res)
 				suite.Require().Empty(session)
+				suite.Require().True(strings.Contains(err.Error(), tc.errArgs.contains))
+			}
+
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestLogonAcceptor() {
+	type args struct {
+		session             types.Sessions
+		msgRegisterAccount1 types.MsgRegisterAccount
+		msgLogonAcceptor    types.MsgLogonAcceptor
+	}
+
+	type errArgs struct {
+		shouldPass bool
+		contains   string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		errArgs errArgs
+	}{
+		{
+			"Logon Request can be Accepted",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.LogonRequestStatus,
+					IsAccepted:    false,
+				},
+				msgRegisterAccount1: types.MsgRegisterAccount{
+					Creator:          suite.address[1].String(),
+					Address:          suite.address[1].String(),
+					CompanyName:      "cypherCore",
+					Website:          "cypherCore.io",
+					SocialMediaLinks: "@cypherCore",
+				},
+				msgLogonAcceptor: *types.NewMsgLogonAcceptor(suite.address[1].String(), "sessionID1", types.LogonAcceptor{
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[0].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					EncryptMethod: 1,
+					HeartBtInt:    1,
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				}),
+			},
+			errArgs{
+				shouldPass: true,
+				contains:   "",
+			},
+		},
+		{
+			"Same Address can not used for SenderCompID & TargetCompID",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.LogonRequestStatus,
+					IsAccepted:    false,
+				},
+				msgRegisterAccount1: types.MsgRegisterAccount{
+					Creator:          suite.address[1].String(),
+					Address:          suite.address[1].String(),
+					CompanyName:      "cypherCore",
+					Website:          "cypherCore.io",
+					SocialMediaLinks: "@cypherCore",
+				},
+				msgLogonAcceptor: *types.NewMsgLogonAcceptor(suite.address[1].String(), "sessionID1", types.LogonAcceptor{
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[1].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					EncryptMethod: 1,
+					HeartBtInt:    1,
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				}),
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"Wrong SessionID",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID2",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.LogonRequestStatus,
+					IsAccepted:    false,
+				},
+				msgRegisterAccount1: types.MsgRegisterAccount{
+					Creator:          suite.address[1].String(),
+					Address:          suite.address[1].String(),
+					CompanyName:      "cypherCore",
+					Website:          "cypherCore.io",
+					SocialMediaLinks: "@cypherCore",
+				},
+				msgLogonAcceptor: *types.NewMsgLogonAcceptor(suite.address[1].String(), "sessionID1", types.LogonAcceptor{
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[0].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					EncryptMethod: 1,
+					HeartBtInt:    1,
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				}),
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"Logon Request has been Accepted",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.LoggedInStatus,
+					IsAccepted:    true,
+				},
+				msgRegisterAccount1: types.MsgRegisterAccount{
+					Creator:          suite.address[1].String(),
+					Address:          suite.address[1].String(),
+					CompanyName:      "cypherCore",
+					Website:          "cypherCore.io",
+					SocialMediaLinks: "@cypherCore",
+				},
+				msgLogonAcceptor: *types.NewMsgLogonAcceptor(suite.address[1].String(), "sessionID1", types.LogonAcceptor{
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[0].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					EncryptMethod: 1,
+					HeartBtInt:    1,
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				}),
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"Logon Request has been rejected",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID1",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{},
+					Status:        constants.RejectedStatus,
+					IsAccepted:    false,
+				},
+				msgRegisterAccount1: types.MsgRegisterAccount{
+					Creator:          suite.address[1].String(),
+					Address:          suite.address[1].String(),
+					CompanyName:      "cypherCore",
+					Website:          "cypherCore.io",
+					SocialMediaLinks: "@cypherCore",
+				},
+				msgLogonAcceptor: *types.NewMsgLogonAcceptor(suite.address[1].String(), "sessionID1", types.LogonAcceptor{
+					Header: &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "A",
+						SenderCompID: suite.address[1].String(),
+						TargetCompID: suite.address[0].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					EncryptMethod: 1,
+					HeartBtInt:    1,
+					Trailer: &types.Trailer{
+						CheckSum: 10,
+					},
+				}),
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+
+			// set session
+			suite.fixKeeper.SetSessions(suite.ctx, tc.args.session.SessionID, tc.args.session)
+
+			// call RegisterAccount method
+			suite.msgServer.RegisterAccount(sdk.WrapSDKContext(suite.ctx), &tc.args.msgRegisterAccount1)
+
+			// call LogonAcceptor method
+			res, err := suite.msgServer.LogonAcceptor(sdk.WrapSDKContext(suite.ctx), &tc.args.msgLogonAcceptor)
+			_, found := suite.fixKeeper.GetSessions(suite.ctx, tc.args.msgLogonAcceptor.SessionID)
+
+			if tc.errArgs.shouldPass {
+				suite.Require().True(found)
+				suite.Require().NoError(err, tc.name)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err, tc.name)
+				suite.Require().Nil(res)
 				suite.Require().True(strings.Contains(err.Error(), tc.errArgs.contains))
 			}
 
