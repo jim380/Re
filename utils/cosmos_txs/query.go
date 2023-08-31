b11d0b37bd2d03936f123730a02e2a559a5665c8
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-08-31 12:09:21 +0100

    added Delegate Txs

--- utils/cosmos_txs/query.go
+++ utils/cosmos_txs/query.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	cosmosHubTxsAPI = "http://localhost:5001/cosmos/txs?limit=10"
+	cosmosHubTxsAPI = "http://localhost:5001/cosmos/txs?limit=20"
 	cacheTTL        = 5 * time.Second
 	fetchEvery      = 5 * time.Second
 	CacheKey        = "cosmosHub"
@@ -30,19 +30,19 @@ type Transaction struct {
 }
 
 type Message struct {
-	Type        string          `json:"@type"`
-	FromAddress string          `json:"from_address,omitempty"`
-	ToAddress   string          `json:"to_address,omitempty"`
-	Amount      json.RawMessage `json:"amount,omitempty"`
-	Token       Coin            `json:"token,omitempty"`
-	ProposalID  string          `json:"proposal_id,omitempty"`
-	Voter       string          `json:"voter,omitempty"`
-	Option      string          `json:"option,omitempty"`
-	Delegator   string          `json:"delegator_address,omitempty"`
-	Validator   string          `json:"validator_address,omitempty"`
-	Sender      string          `json:"sender,omitempty"`
-	Receiver    string          `json:"receiver,omitempty"`
-	Msgs        []Msg           `json:"msgs,omitempty"`
+	Type             string          `json:"@type"`
+	FromAddress      string          `json:"from_address,omitempty"`
+	ToAddress        string          `json:"to_address,omitempty"`
+	Amount           json.RawMessage `json:"amount,omitempty"`
+	Token            Coin            `json:"token,omitempty"`
+	ProposalID       string          `json:"proposal_id,omitempty"`
+	Voter            string          `json:"voter,omitempty"`
+	Option           string          `json:"option,omitempty"`
+	DelegatorAddress string          `json:"delegator_address,omitempty"`
+	ValidatorAddress string          `json:"validator_address,omitempty"`
+	Sender           string          `json:"sender,omitempty"`
+	Receiver         string          `json:"receiver,omitempty"`
+	Msgs             []Msg           `json:"msgs,omitempty"`
 }
 
 type Msg struct {
