440d9a46f1ebd0e95be795040cd693ee8a607934
Ikpia <echukwuka97@gmail.com>
2023-09-01 11:07:54 -0700

    added keeper test to oracle

---
+++ x/oracle/keeper/keeper_test.go
@@ -0,0 +1,46 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	reapp "github.com/jim380/Re/app"
+	fixKeeper "github.com/jim380/Re/x/fix/keeper"
+	"github.com/jim380/Re/x/oracle/types"
+	oracleKeeper "github.com/jim380/Re/x/oracle/keeper"
+	"github.com/stretchr/testify/suite"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+var (
+	address1 = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	adress2  = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	address    = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+)
+
+// shared setup
+type KeeperTestSuite struct {
+	suite.Suite
+
+	address   []sdk.AccAddress
+	app       *reapp.ReApp
+	ctx       sdk.Context
+	fixKeeper fixKeeper.Keeper
+	oracleKeeper oracleKeeper.Keeper
+	msgServer types.MsgServer
+}
+
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.app = reapp.Setup(false)
+	suite.ctx = suite.app.BaseApp.NewContext(false, tmproto.Header{})
+	suite.fixKeeper = suite.app.FixKeeper
+	suite.oracleKeeper = suite.app.OracleKeeper
+	suite.msgServer = fixKeeper.NewMsgServerImpl(suite.app.FixKeeper)
+
+	suite.address = []sdk.AccAddress{address1, address2, address}
+}
+
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
+}
