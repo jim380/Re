dd1504239a40e15a30fe792047cdcaa967bacfa7
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-09-14 04:22:07 +0100

    bitcoin struct

--- utils/bitcoin/query.go
+++ utils/bitcoin/query.go
@@ -1,17 +1,26 @@
 package bitcoin
 
+import "time"
+
+const (
+	bitcoinTxsAPI = "http://localhost:5001/bitcoin/txs?limit=15"
+	cacheTTL      = 5 * time.Second
+	fetchEvery    = 5 * time.Second
+	CacheKey      = "bitcoin"
+)
+
 type Input struct {
 	PrevAddresses []string `json:"prev_addresses"`
 	PrevPosition  int      `json:"prev_position"`
 	PrevTxHash    string   `json:"prev_tx_hash"`
 	PrevType      string   `json:"prev_type"`
-	PrevValue     int64    `json:"prev_value"`
-	Sequence      uint32   `json:"sequence"`
+	PrevValue     int      `json:"prev_value"`
+	Sequence      int      `json:"sequence"`
 }
 
 type Output struct {
 	Addresses         []string `json:"addresses"`
-	Value             int64    `json:"value"`
+	Value             int      `json:"value"`
 	Type              string   `json:"type"`
 	SpentByTx         string   `json:"spent_by_tx"`
 	SpentByTxPosition int      `json:"spent_by_tx_position"`
@@ -19,30 +28,16 @@ type Output struct {
 
 type Transaction struct {
 	BlockHeight   int      `json:"block_height"`
-	BlockHash     string   `json:"block_hash"`
-	BlockTime     int64    `json:"block_time"`
-	CreatedAt     int64    `json:"created_at"`
-	Confirmations int      `json:"confirmations"`
-	Fee           int64    `json:"fee"`
 	Hash          string   `json:"hash"`
-	InputsCount   int      `json:"inputs_count"`
-	InputsValue   int64    `json:"inputs_value"`
-	IsCoinbase    bool     `json:"is_coinbase"`
+	BlockTime     string   `json:"block_time"`
+	Fee           int      `json:"fee"`
 	IsDoubleSpend bool     `json:"is_double_spend"`
-	IsSwTx        bool     `json:"is_sw_tx"`
-	LockTime      int      `json:"lock_time"`
 	OutputsCount  int      `json:"outputs_count"`
-	OutputsValue  int64    `json:"outputs_value"`
-	Sigops        int      `json:"sigops"`
-	Size          int      `json:"size"`
-	Version       int      `json:"version"`
-	//Vsize         int      `json:"vsize"`
-	Weight        int      `json:"weight"`
-	WitnessHash   string   `json:"witness_hash"`
+	OutputsValue  int      `json:"outputs_value"`
 	Inputs        []Input  `json:"inputs"`
 	Outputs       []Output `json:"outputs"`
 }
 
 type TransactionResponse struct {
-	Transactions []Transaction `json:"list"`
+	Transactions []Transaction `json:"transactions"`
 }
