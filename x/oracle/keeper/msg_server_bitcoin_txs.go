3224ddd1a4e85b12a3d1754c617b6f7d0703e3f5
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-09-16 21:37:49 +0100

    logon session for bitcoin transactions

--- x/oracle/keeper/msg_server_bitcoin_txs.go
+++ x/oracle/keeper/msg_server_bitcoin_txs.go
@@ -2,10 +2,13 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/jim380/Re/utils/bitcoin"
 	"github.com/jim380/Re/utils/constants"
+	fixTypes "github.com/jim380/Re/x/fix/types"
 	"github.com/jim380/Re/x/oracle/types"
 )
 
@@ -24,6 +27,47 @@ func (k msgServer) BitcoinTxs(goCtx context.Context, msg *types.MsgBitcoinTxs) (
 	// set Bitcoin txs, map to the fix module
 	// update fix module from oracle
 	// start refetching data every 5 seconds and return latest Bitcoin Txs
+	go bitcoin.RefetchBitcoinTxsDataPeriodically(constants.BitcoinCacheKey)
+	transactions, err := bitcoin.GetCachedBitcoinTransactions(constants.BitcoinCacheKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to return queried chain transactions data: %w", err)
+	}
+	for _, tx := range transactions.Transactions {
+		// set sessions for parties existing in the transactions
+		session := fixTypes.Sessions{
+			SessionID: tx.Hash,
+			LogonInitiator: &fixTypes.LogonInitiator{
+				Header: &fixTypes.Header{
+					BeginString:  "fix4.4",
+					MsgType:      "A",
+					SenderCompID: "",
+					TargetCompID: "",
+					MsgSeqNum:    int64(tx.BlockHeight),
+					SendingTime:  tx.BlockTime,
+				},
+				Trailer: &fixTypes.Trailer{
+					CheckSum: 0,
+				},
+			},
+			LogonAcceptor: &fixTypes.LogonAcceptor{
+				Header: &fixTypes.Header{
+					BeginString:  "fix4.4",
+					MsgType:      "A",
+					SenderCompID: "",
+					TargetCompID: "",
+					MsgSeqNum:    int64(tx.BlockHeight),
+					SendingTime:  tx.BlockTime,
+				},
+				Trailer: &fixTypes.Trailer{
+					CheckSum: 0,
+				},
+			},
+			Status:     "loggedIn",
+			IsAccepted: true,
+		}
+		// set new session to store
+		k.fixKeeper.SetSessions(ctx, tx.Hash, session)
+	}
 
 	oracle := types.MultiChainTxOracle{
 		OracleID:  msg.OracleID,
