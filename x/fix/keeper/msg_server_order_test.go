fe87d935b8541a74acc56f21945d32a8aab2cf79
Ikpia <echukwuka97@gmail.com>
2023-09-23 13:16:26 -0700

    added test for order cancel request

--- x/fix/keeper/msg_server_order_test.go
+++ x/fix/keeper/msg_server_order_test.go
@@ -190,3 +190,238 @@ func (suite *KeeperTestSuite) TestNewOrderSingle() {
 		})
 	}
 }
+
+func (suite *KeeperTestSuite) TestOrderCancelRequest() {
+	type args struct {
+		session     		  types.Sessions
+		msgOrderCancelRequest types.MsgOrderCancelRequest
+		newOrderCancelRequest types.OrdersCancelRequest
+	}
+
+	type errArgs struct {
+		shouldPass bool
+		contains   string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		errArgs errArgs
+	}{
+		{
+			"Valid Order Cancel Request",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "F",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "F",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
+				msgOrderCancelRequest: *types.NewMsgOrderCancelRequest(suite.address[0].String(), "sessionID", "origClOrdID", "clOrdID"),
+				newOrderCancelRequest: types.OrdersCancelRequest{
+					SessionID:   "sessionID",
+					Header:      &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "F",
+						SenderCompID: suite.address[0].String(),
+						TargetCompID: suite.address[1].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					OrigClOrdID: "origClOrdID",
+					ClOrdID:     "clOrdID",
+					Trailer:     &types.Trailer{
+						CheckSum: 10,
+					},
+				},
+			},
+			errArgs{
+				shouldPass: true,
+				contains:   "",
+			},
+		},
+		{
+			"Account address is not the creator of the order",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "F",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "F",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
+				msgOrderCancelRequest: *types.NewMsgOrderCancelRequest(suite.address[4].String(), "sessionID", "origClOrdID", "clOrdID"),
+				newOrderCancelRequest: types.OrdersCancelRequest{
+					SessionID:   "sessionID",
+					Header:      &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "F",
+						SenderCompID: suite.address[0].String(),
+						TargetCompID: suite.address[1].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					OrigClOrdID: "origClOrdID",
+					ClOrdID:     "clOrdID",
+					Trailer:     &types.Trailer{
+						CheckSum: 10,
+					},
+			},
+		},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"The sessionID does not exist",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "F",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "F",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
+				msgOrderCancelRequest: *types.NewMsgOrderCancelRequest(suite.address[0].String(), "sessionIDqwerrty", "origClOrdID", "clOrdID"),
+				newOrderCancelRequest: types.OrdersCancelRequest{
+					SessionID:   "sessionID",
+					Header:      &types.Header{
+						BeginString:  "FIX4.4",
+						BodyLength:   10,
+						MsgType:      "F",
+						SenderCompID: suite.address[0].String(),
+						TargetCompID: suite.address[1].String(),
+						MsgSeqNum:    1,
+						SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+					},
+					OrigClOrdID: "origClOrdID",
+					ClOrdID:     "clOrdID",
+					Trailer:     &types.Trailer{
+						CheckSum: 10,
+					},
+				},
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+	}
+
+
+	for _, tc := range tests {
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+			// set session
+			suite.fixKeeper.SetSessions(tc.args.session.SessionID, tc.args.session)
+			// set order cancel request
+			suite.fixKeeper.SetOrdersCancelRequest(suite.ctx, tc.args.msgOrderCancelRequest.ClOrdID, tc.args.newOrderCancelRequest)
+
+			// call Order cancel request method
+			res, err := suite.msgServer.OrderCancelRequest(sdk.WrapSDKContext(suite.ctx), &tc.args.msgOrderCancelRequest)
+
+			if tc.errArgs.shouldPass {
+				suite.Require().NoError(err, tc.name)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err, tc.name)
+				suite.Require().Nil(res)
+				suite.Require().True(strings.Contains(err.Error(), tc.errArgs.contains))
+			}
+		})
+	}
+}
