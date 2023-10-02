f9ba2817b17aa54c72ffadf56d30ed77d7136609
Ikpia <echukwuka97@gmail.com>
2023-10-02 10:10:55 -0700

    wrote tests for security types request

---
+++ x/fix/keeper/msg_server_security_types_test.go
@@ -0,0 +1,237 @@
+package keeper_test
+
+import (
+	"strings"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/jim380/Re/utils/constants"
+	"github.com/jim380/Re/x/fix/types"
+)
+
+func (suite *KeeperTestSuite) TestSecurityTypesRequest() {
+	type args struct {
+		session                 types.Sessions
+		msgSecurityTypesRequest types.MsgSecurityTypesRequest
+	}
+
+	type errArgs struct {
+		shouldPass bool
+		contains   string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		errArgs errArgs
+	}{
+		{
+			"Valid Security Type Request",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
+				msgSecurityTypesRequest: *types.NewMsgSecurityTypesRequest(suite.address[0].String(), "sessionID", "securityReqID", "text", "tradingSessionID", "tradingSessionSubID", "product", "securityType", "securitySubType"),
+			},
+			errArgs{
+				shouldPass: true,
+				contains:   "",
+			},
+		},
+		{
+			"logon status is not equals to loggedIn",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     "logged_out",
+					IsAccepted: true,
+				},
+				msgSecurityTypesRequest: *types.NewMsgSecurityTypesRequest(suite.address[0].String(), "sessionID", "securityReqID", "text", "tradingSessionID", "tradingSessionSubID", "product", "securityType", "securitySubType"),
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"Parties involved in a session are not the ones using the sessionID and are not able to create Security Types Request",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
+				msgSecurityTypesRequest: *types.NewMsgSecurityTypesRequest(suite.address[2].String(), "sessionID", "securityReqID", "text", "tradingSessionID", "tradingSessionSubID", "product", "securityType", "securitySubType"),
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+		{
+			"The sessionID does not exist",
+			args{
+				session: types.Sessions{
+					SessionID: "sessionID",
+					LogonInitiator: &types.LogonInitiator{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[0].String(),
+							TargetCompID: suite.address[1].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					LogonAcceptor: &types.LogonAcceptor{
+						Header: &types.Header{
+							BeginString:  "FIX4.4",
+							BodyLength:   10,
+							MsgType:      "A",
+							SenderCompID: suite.address[1].String(),
+							TargetCompID: suite.address[0].String(),
+							MsgSeqNum:    1,
+							SendingTime:  time.Now().Format("2006-01-02 15:04:05"),
+						},
+						EncryptMethod: 1,
+						HeartBtInt:    1,
+						Trailer: &types.Trailer{
+							CheckSum: 10,
+						},
+					},
+					Status:     constants.LoggedInStatus,
+					IsAccepted: true,
+				},
+				msgSecurityTypesRequest: *types.NewMsgSecurityTypesRequest(suite.address[0].String(), "sessionIDjjfefje", "securityReqID", "text", "tradingSessionID", "tradingSessionSubID", "product", "securityType", "securitySubType"),
+			},
+			errArgs{
+				shouldPass: false,
+				contains:   "",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+			// set session
+			suite.fixKeeper.SetSessions(suite.ctx, tc.args.session.SessionID, tc.args.session)
+
+			// call  Security Types Request
+			res, err := suite.msgServer.SecurityTypesRequest(sdk.WrapSDKContext(suite.ctx), &tc.args.msgSecurityTypesRequest)
+
+			if tc.errArgs.shouldPass {
+				suite.Require().NoError(err, tc.name)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err, tc.name)
+				suite.Require().Nil(res)
+				suite.Require().True(strings.Contains(err.Error(), tc.errArgs.contains))
+			}
+		})
+	}
+}
